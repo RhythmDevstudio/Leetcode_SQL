93b083c1e5251a4fe90d6e51b2275b469a57b78d
RHYTHM MISHRA <125253220+RhythmDevstudio@users.noreply.github.com>
2025-10-21 15:24:26 +0530

    Time: 550 ms (37.25%), Space: 0B (100%) - LeetHub

--- 3642-find-books-with-polarized-opinions/3642-find-books-with-polarized-opinions.sql
+++ 3642-find-books-with-polarized-opinions/3642-find-books-with-polarized-opinions.sql
@@ -9,25 +9,35 @@
 -- using(book_id) where polarization_score >= 0.6
 -- order by polarization_score desc, title desc;
 
-with book_stats as(
-    select book_id,
-    count(*)total_session,
-    min(session_rating)minrating,
-    max(session_rating)maxrating,
-    sum(case when session_rating>3 then 1 else 0 end)highrating,
-    sum(case when session_rating<3 then 1 else 0 end )lowrating,
-    sum(case when session_rating>3 or session_rating<3 then 1 else 0 end) extremes
-    from reading_sessions 
-    group by 1
-)
-select book_id,title,author,genre,pages,
-maxrating-minrating rating_spread ,
-round(extremes*1.0/total_session,2)polarization_score 
- from book_stats
-join books
-using(book_id)
-where total_session >4
-and highrating>0
-and lowrating>0
-having polarization_score>=0.6
-order by polarization_score desc,title desc;
+-- with book_stats as(
+--     select book_id,
+--     count(*)total_session,
+--     min(session_rating)minrating,
+--     max(session_rating)maxrating,
+--     sum(case when session_rating>3 then 1 else 0 end)highrating,
+--     sum(case when session_rating<3 then 1 else 0 end )lowrating,
+--     sum(case when session_rating>3 or session_rating<3 then 1 else 0 end) extremes
+--     from reading_sessions 
+--     group by 1
+-- )
+-- select book_id,title,author,genre,pages,
+-- maxrating-minrating rating_spread ,
+-- round(extremes*1.0/total_session,2)polarization_score 
+--  from book_stats
+-- join books
+-- using(book_id)
+-- where total_session >4
+-- and highrating>0
+-- and lowrating>0
+-- having polarization_score>=0.6
+-- order by polarization_score desc,title desc;
+
+# Write your MySQL query statement below
+SELECT  r.book_id, b.title, b.author, b.genre, b.pages, MAX(r.session_rating) - MIN(r.session_rating) rating_spread, 
+round( (SUM( CASE WHEN r.session_rating < 3 THEN 1 ELSE 0 END + CASE WHEN r.session_rating > 3 THEN 1 ELSE 0 END)) / COUNT(r.session_id) , 2) polarization_score
+FROM reading_sessions r
+INNER JOIN books b USING(book_id)
+GROUP BY r.book_id, b.title, b.author, b.genre, b.pages
+HAVING COUNT(r.session_id) > 4 AND MAX(r.session_rating) > 3 AND MIN(r.session_rating) < 3 AND
+polarization_score >= 0.6
+ORDER BY polarization_score DESC, b.title DESC;
