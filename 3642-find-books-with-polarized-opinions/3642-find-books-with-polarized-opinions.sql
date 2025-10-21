a355e22adcf5a5c6d9376e68f650c2955045f007
RHYTHM MISHRA <125253220+RhythmDevstudio@users.noreply.github.com>
2025-10-21 15:23:34 +0530

    Time: 500 ms (72.92%), Space: 0B (100%) - LeetHub

--- 3642-find-books-with-polarized-opinions/3642-find-books-with-polarized-opinions.sql
+++ 3642-find-books-with-polarized-opinions/3642-find-books-with-polarized-opinions.sql
@@ -1,10 +1,33 @@
 # Write your MySQL query statement below
-select * from books JOIN (
-    select book_id, 
-    max(session_rating) - min(session_rating) as rating_spread, 
-    ROUND(sum(session_rating != 3)/count(*), 2) as polarization_score
-    from reading_sessions group by book_id 
-    having count(*) >= 5 and max(session_rating) >= 4 and min(session_rating) <= 2
-)T
-using(book_id) where polarization_score >= 0.6
-order by polarization_score desc, title desc;
+-- select * from books JOIN (
+--     select book_id, 
+--     max(session_rating) - min(session_rating) as rating_spread, 
+--     ROUND(sum(session_rating != 3)/count(*), 2) as polarization_score
+--     from reading_sessions group by book_id 
+--     having count(*) >= 5 and max(session_rating) >= 4 and min(session_rating) <= 2
+-- )T
+-- using(book_id) where polarization_score >= 0.6
+-- order by polarization_score desc, title desc;
+
+with book_stats as(
+    select book_id,
+    count(*)total_session,
+    min(session_rating)minrating,
+    max(session_rating)maxrating,
+    sum(case when session_rating>3 then 1 else 0 end)highrating,
+    sum(case when session_rating<3 then 1 else 0 end )lowrating,
+    sum(case when session_rating>3 or session_rating<3 then 1 else 0 end) extremes
+    from reading_sessions 
+    group by 1
+)
+select book_id,title,author,genre,pages,
+maxrating-minrating rating_spread ,
+round(extremes*1.0/total_session,2)polarization_score 
+ from book_stats
+join books
+using(book_id)
+where total_session >4
+and highrating>0
+and lowrating>0
+having polarization_score>=0.6
+order by polarization_score desc,title desc;
