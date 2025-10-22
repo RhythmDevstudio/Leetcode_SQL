7afb06888e41e98f88928f4fe97e5e7f3caabe81
RHYTHM MISHRA <125253220+RhythmDevstudio@users.noreply.github.com>
2025-10-22 15:30:30 +0530

    Time: 290 ms (37.41%), Space: 0B (100%) - LeetHub

--- 3475-dna-pattern-recognition/3475-dna-pattern-recognition.sql
+++ 3475-dna-pattern-recognition/3475-dna-pattern-recognition.sql
@@ -1,8 +1,18 @@
 # Write your MySQL query statement below
-select  sample_id, dna_sequence, species, 
-   (case when dna_sequence like "ATG%" then 1  else 0 end) as has_start,
-   (case when regexp_like (dna_sequence,"TAA$|TAG$|TGA$") then 1 else 0 end) as has_stop,
-   (case when dna_sequence like "%ATAT%" then 1 else 0 end) as has_atat,
-   (case when dna_sequence like "%GGG%" then 1 else 0 end) as has_ggg
-from Samples
-group by sample_id;
+-- select  sample_id, dna_sequence, species, 
+--    (case when dna_sequence like "ATG%" then 1  else 0 end) as has_start,
+--    (case when regexp_like (dna_sequence,"TAA$|TAG$|TGA$") then 1 else 0 end) as has_stop,
+--    (case when dna_sequence like "%ATAT%" then 1 else 0 end) as has_atat,
+--    (case when dna_sequence like "%GGG%" then 1 else 0 end) as has_ggg
+-- from Samples
+-- group by sample_id;
+
+# Write your MySQL query statement below
+SELECT
+    *,
+    dna_sequence REGEXP '^ATG' AS has_start,
+    dna_sequence REGEXP 'TAA$|TAG$|TGA$' AS has_stop,
+    dna_sequence REGEXP 'ATAT' AS has_atat,
+    dna_sequence REGEXP 'GGG' AS has_ggg
+FROM samples
+ORDER BY sample_id;
